a32502454a6c70bf43ae61eb1b71e6d3b6bf1e24
Alexia Legrand <alegrand@odialab.com>
2022-08-26 17:28:29 +0200

    Add mod

---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace DecryptedSaves
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+            // Patch save method
+            harmony.Patch(
+                AccessTools.Method(typeof(SaveAndLoad), "Saving"),
+                null,
+                new HarmonyMethod(AccessTools.Method(typeof(Plugin), "PreventEncryption"))
+                );
+
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPostfix]
+        public static void PreventEncryption(SaveAndLoad __instance)
+        {
+            COTLDataReadWriter<DataManager> _saveFileReadWriter = Traverse.Create(Singleton<SaveAndLoad>.Instance).Field("_saveFileReadWriter").GetValue() as COTLDataReadWriter<DataManager>;
+            _saveFileReadWriter.Write(DataManager.Instance, SaveAndLoad.MakeSaveSlot(SaveAndLoad.SAVE_SLOT), false, true);
+        }
+
+    }
+
+}
